60253dc811c7e0e6360e888db4efc428a5f2e5fa
osdnk <micosa97@gmail.com>
2018-06-03 01:57:17 +0200

    Add interpolation mechanism

---
+++ src/splineInterpolation.js
@@ -0,0 +1,39 @@
+import Animated from "react-native-reanimated";
+import { creteInterpolationSplines } from "./reanimatedSplineInterpolation";
+
+const { cond, add, multiply, lessThan, lessOrEq, sub, pow } = Animated;
+
+function splineInterpolate(V, X, Y) {
+  const { as, bs, cs, ds } = creteInterpolationSplines(X, Y);
+  const n = X.length - 1;
+  let h = sub(V, X[n - 1]);
+  let t = cond(
+    lessOrEq(V, X[n]),
+    add(
+      as[n - 1],
+      multiply(bs[n - 1], h),
+      multiply(cs[n - 1], pow(h, 2), 0.5),
+      multiply(ds[n - 1], pow(h, 3), 1 / 6)
+    ),
+    0
+  );
+  for (let i = n - 1; i >= 1; i--) {
+    h = sub(V, X[i - 1]);
+    t = cond(
+      lessThan(V, X[i]),
+      add(
+        as[i - 1],
+        multiply(bs[i - 1], h),
+        multiply(cs[i - 1], pow(h, 2), 0.5),
+        multiply(ds[i - 1], pow(h, 3), 1 / 6)
+      ),
+      t
+    );
+  }
+  t = cond(lessThan(V, X[0]), 0, t);
+  return t;
+}
+
+module.exports = {
+  splineInterpolate
+};
