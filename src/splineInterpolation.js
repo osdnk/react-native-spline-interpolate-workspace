d4b8f996f18d6a4220dcdd65105999265b63da66
osdnk <micosa97@gmail.com>
2018-06-03 02:16:45 +0200

    Rename modules properly
    (cherry picked from commit 5bc43e7923292c1af7cd382de5f13c652366b591)

--- src/splineInterpolation.js
+++ src/splineInterpolation.js
@@ -1,39 +1,111 @@
-import Animated from "react-native-reanimated";
-import { creteInterpolationSplines } from "./reanimatedSplineInterpolation";
-
-const { cond, add, multiply, lessThan, lessOrEq, sub, pow } = Animated;
-
-function splineInterpolate(V, X, Y) {
-  const { as, bs, cs, ds } = creteInterpolationSplines(X, Y);
-  const n = X.length - 1;
-  let h = sub(V, X[n - 1]);
-  let t = cond(
-    lessOrEq(V, X[n]),
-    add(
-      as[n - 1],
-      multiply(bs[n - 1], h),
-      multiply(cs[n - 1], pow(h, 2), 0.5),
-      multiply(ds[n - 1], pow(h, 3), 1 / 6)
-    ),
-    0
-  );
-  for (let i = n - 1; i >= 1; i--) {
-    h = sub(V, X[i - 1]);
-    t = cond(
-      lessThan(V, X[i]),
-      add(
-        as[i - 1],
-        multiply(bs[i - 1], h),
-        multiply(cs[i - 1], pow(h, 2), 0.5),
-        multiply(ds[i - 1], pow(h, 3), 1 / 6)
-      ),
-      t
-    );
+function gaussElimination(A, Y) {
+  const sizeOfArray = A.length;
+  for (let i = 0; i < sizeOfArray; i++) {
+    let maxElemInColumn = Math.abs(A[i][i]),
+      maxRow = i;
+    for (let k = i + 1; k < sizeOfArray; k++) {
+      if (Math.abs(A[k][i]) > maxElemInColumn) {
+        maxElemInColumn = Math.abs(A[k][i]);
+        maxRow = k;
+      }
+    }
+    for (let k = i; k < sizeOfArray; k++) {
+      [A[i][k], A[maxRow][k]] = [A[maxRow][k], A[i][k]];
+    }
+    [Y[maxRow], Y[i]] = [Y[i], Y[maxRow]];
+    for (let k = i + 1; k < sizeOfArray; k++) {
+      const diff = -A[k][i] / A[i][i];
+      for (let j = i; j < sizeOfArray; j++) {
+        if (i === j) {
+          A[k][j] = 0;
+        } else {
+          A[k][j] += diff * A[i][j];
+        }
+      }
+      Y[k] += diff * Y[i];
+    }
   }
-  t = cond(lessThan(V, X[0]), 0, t);
-  return t;
-}
 
-module.exports = {
-  splineInterpolate
-};
+  const results = [];
+  for (let i = sizeOfArray - 1; i > -1; i--) {
+    results[i] = Y[i] / A[i][i];
+    for (let k = i - 1; k > -1; k--) {
+      Y[k] -= A[k][i] * results[i];
+    }
+  }
+
+  return results;
+}
+export function creteInterpolationSplines(inputValues, outputValues) {
+  const interpolationNodes = [];
+  for (let i = 0; i < inputValues.length; i++) {
+    interpolationNodes.push({ x: inputValues[i], y: outputValues[i] });
+  }
+
+  const n = inputValues.length - 1;
+  const deltas = [];
+  for (let i = 0; i <= n - 1; i++) {
+    deltas[i] =
+      (interpolationNodes[i + 1].y - interpolationNodes[i].y) /
+      (interpolationNodes[i + 1].x - interpolationNodes[i].x);
+  }
+  const thetas = [];
+  for (let i = 0; i <= n - 2; i++) {
+    thetas[i] =
+      (deltas[i + 1] - deltas[i]) /
+      (interpolationNodes[i + 2].x - interpolationNodes[i].x);
+  }
+
+  const h = [];
+  for (let i = 0; i <= n - 1; i++) {
+    h[i] = interpolationNodes[i + 1].x - interpolationNodes[i].x;
+  }
+
+  const u = [];
+  const w = [];
+  for (let i = 1; i <= n - 1; i++) {
+    u[i] = h[i - 1] / (h[i - 1] + h[i]);
+    w[i] = h[i] / (h[i - 1] + h[i]);
+  }
+
+  const A = [];
+  for (let i = 0; i <= n - 2; i++) {
+    A[i] = [];
+    for (let j = 0; j <= n - 2; j++) {
+      if (i === j) A[i][j] = 2;
+      else if (i === j + 1) A[i][j] = u[i + 1];
+      else if (i + 1 === j) A[i][j] = w[j];
+      else A[i][j] = 0;
+    }
+  }
+
+  const r = [0].concat(gaussElimination(A, thetas)).concat([0]);
+  const as = [];
+  const bs = [];
+  const cs = [];
+  const ds = [];
+
+  for (let i = 0; i <= n; i++) {
+    cs[i] = r[i] * 6;
+  }
+  for (let i = 0; i <= n - 1; i++) {
+    as[i] = interpolationNodes[i].y;
+    bs[i] = deltas[i] - h[i] * (cs[i + 1] + 2 * cs[i]) / 6;
+    ds[i] = (cs[i + 1] - cs[i]) / h[i];
+  }
+  for (let i = 0; i <= n - 1; i++) {
+    let p =
+      as[i] +
+      bs[i] * (interpolationNodes[i + 1].x - interpolationNodes[i].x) +
+      cs[i] *
+        (interpolationNodes[i + 1].x - interpolationNodes[i].x) *
+        (interpolationNodes[i + 1].x - interpolationNodes[i].x) /
+        2 +
+      ds[i] *
+        (interpolationNodes[i + 1].x - interpolationNodes[i].x) *
+        (interpolationNodes[i + 1].x - interpolationNodes[i].x) *
+        (interpolationNodes[i + 1].x - interpolationNodes[i].x) /
+        6;
+  }
+  return { as, bs, cs, ds };
+}
